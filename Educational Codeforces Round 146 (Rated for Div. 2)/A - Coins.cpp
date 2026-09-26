46670590c818fa1a270e68aafc9fbc11f1282dd1
Sujeet Kumar <sujeet901626@gmail.com>
2026-09-26 07:55:36 +0530

    Create A - Coins.cpp

---
+++ Educational Codeforces Round 146 (Rated for Div. 2)/A - Coins.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  long long n, k;
+  cin >> n >> k;
+  if (n % 2 == 0 || (n - k) % 2 == 0) cout << "YES\n";
+  else cout << "NO\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
